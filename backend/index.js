c18b64de9a6dbd91af1d7a40337ae875e42bde03
prateek <itsprateek0444@gmail.com>
2025-06-16 15:51:04 +0530

    chaned  env

--- backend/index.js
+++ backend/index.js
@@ -17,13 +17,7 @@ const MONGO_URL = process.env.MONGO_URI;
 //middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+app.use(cors());
 
 app.use(
   fileUpload({
